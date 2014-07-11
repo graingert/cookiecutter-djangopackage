0c5a917839b7e4a7f187fb81f182c93c0d9be83d
Thomas Grainger <tagrain@gmail.com>
2014-07-11 13:55:03 +0100

    Add setup.py customizations and fixes

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,52 +4,33 @@
 import os
 import sys
 
-import {{ cookiecutter.app_name }}
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-version = {{ cookiecutter.app_name }}.__version__
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
+from setuptools import setup, find_packages
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='{{ cookiecutter.project_name }}',
-    version=version,
+    version='{{ cookiecutter.version }}',
     description="""{{ cookiecutter.project_short_description }}""",
     long_description=readme + '\n\n' + history,
     author='{{ cookiecutter.full_name }}',
     author_email='{{ cookiecutter.email }}',
     url='https://github.hogarthww.prv/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    packages=[
-        '{{ cookiecutter.app_name }}',
-    ],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
     ],
-    license="BSD",
+    license="PROPRIETARY",
     zip_safe=False,
     keywords='{{ cookiecutter.repo_name }}',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'Private :: Do Not Upload',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
     ],
 )
